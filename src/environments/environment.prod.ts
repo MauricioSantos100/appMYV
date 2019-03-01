e67b7bc99e545a87a0efdeb72893c7ff83d972f0
MauricioSantos100 <43189404+MauricioSantos100@users.noreply.github.com>
2019-02-28 21:02:25 -0300

    ultima mudanca

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDkgN9OWwQZMfcslI96ME3dEO5xqoY2_Qw",
+    authDomain: "appmyv-d4625.firebaseapp.com",
+    databaseURL: "https://appmyv-d4625.firebaseio.com",
+    projectId: "appmyv-d4625",
+    storageBucket: "appmyv-d4625.appspot.com",
+    messagingSenderId: "831327464874"
+  }
 };
